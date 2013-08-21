c7f508218a573da94998376910f4320bfb35ab11
Zach Wade <zacharywade@gmail.com>
2013-08-21 11:25:54 -0400

    rotate/delete

--- main.js
+++ main.js
@@ -33,6 +33,11 @@ run = function() {
 			s.append("<br>")
 		}
 	}
+	s.append("<div class='sideaction' style='background-image: url(\"left.png\")' id='tleft'></div>")
+	s.append("<div class='sideaction' style='background-image: url(\"right.png\")' id='tright'></div>")
+	s.append('<br>')
+	s.append("<div class='sideaction' style='background-image: url(\"delete.png\")' id='tdel'></div>")
+
 	$(".cell").click(function(obj) {
 		$(".cell").css("background","#06f")
 		$(this).css("background","#59f")
@@ -46,6 +51,41 @@ run = function() {
 		active.html("<img src='"+$(this).attr('content')+".svg' id='"+d.getTime()+"' class='active'></img>")
 		active.attr("content",$(this).attr('content'))
 	})
+	$("#tright").click(function() {
+		var act = $("#"+$(active.html()).attr('id'))
+		var type = active.attr('content')
+		var deg = act.attr('deg') | 0
+		console.log(act,type,deg)
+		if (deg>=360) {deg-=360}
+		if (type=="mirror" || type=="diag" || type=="oneway" || type=="odiag") {
+			act.css("transform", "rotateZ("+(deg+45)+"deg)")
+			act.attr('deg',deg+45)
+			return
+		}
+		act.css("transform", "rotateZ("+(deg+90)+"deg)")
+		act.attr('deg',deg+90)
+		return
+	})
+	$("#tleft").click(function() {
+		var act = $("#"+$(active.html()).attr('id'))
+		var type = active.attr('content')
+		var deg = act.attr('deg') | 0
+		console.log(act,type,deg)
+		if (deg<=0) {deg+=360}
+		if (type=="mirror" || type=="diag" || type=="oneway" || type=="odiag") {
+			act.css("transform", "rotateZ("+(deg-45)+"deg)")
+			act.attr('deg',deg-45)
+			return
+		}
+		act.css("transform", "rotateZ("+(deg-90)+"deg)")
+		act.attr('deg',deg-90)
+		return
+	})
+	$("#tdel").click(function() {
+		active.html("")
+		active.attr('content',"")
+	})
+
 }
 
 $(document).ready(function() {run()})
